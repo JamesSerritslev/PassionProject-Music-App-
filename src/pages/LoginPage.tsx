30decf3efb7a98d452ca47d53151bd0495145d62
JamesSerritslev <jamesserritslev@mail.weber.edu>
2026-02-10 19:34:07 -0700

    e

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,4 +1,4 @@
-git import { useState } from 'react'
+import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { useAuth } from '@/context/AuthContext'
 import './Auth.css'
